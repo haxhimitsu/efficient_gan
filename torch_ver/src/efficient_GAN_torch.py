5d638e956ac04d0f45906f21eabf0fe3acc7b3ed
haxhimitsu <hachimitsu.lab@gmail.com>
2021-02-05 16:04:38 +0900

    protype worked git add efficient_GAN_torch.py

--- torch_ver/src/efficient_GAN_torch.py
+++ torch_ver/src/efficient_GAN_torch.py
@@ -20,13 +20,23 @@ import copy
 import random
 import argparse
 import sys
-#my module
+import matplotlib.pyplot as plt
 
+#my create original module
 from utils.data_loader import data_loader,GAN_Img_Dataset,ImageTransform
 from utils.gan_model import Generator,Discriminator,Encoder
-data_load=data_loader()
-data_load.sayStr("Hello")
+from train import train
 
+data_load=data_loader()
+#data_load.sayStr("Hello")
+#test=train()
+#test.sayStr("test")
+
+# Setup seeds
+torch.manual_seed(1234)
+torch.cuda.manual_seed(1234)
+np.random.seed(1234)
+random.seed(1234)
 
 
 parser = argparse.ArgumentParser()
@@ -56,9 +66,10 @@ else:
     val_path=a.val_path
 
 #create_data_path_list
-train_img_list,val_img_list=data_load.create_dataset(train_path,val_path)
+print(val_path)
+train_img_list,val_img_list=data_load.create_dataset(train_path=train_path,val_path=val_path)
 #print("train_img_list\n",train_img_list)
-#print("val_img list\n",train_img_list)
+print("val_img list\n",val_img_list)
 
 #create_data_set
 mean=(0.5,)
@@ -66,171 +77,38 @@ std=(0.5,)
 train_dataset=GAN_Img_Dataset(
     file_list=train_img_list,transform=ImageTransform(mean,std)
 )
+test_dataset=GAN_Img_Dataset(
+    file_list=val_img_list,transform=ImageTransform(mean,std)
+)
+
 #create_data_loader
+batch_size=5
+test_dataloader = torch.utils.data.DataLoader(
+    test_dataset, batch_size=batch_size, shuffle=True)
+
 batch_size = 64
 train_dataloader = torch.utils.data.DataLoader(
     train_dataset, batch_size=batch_size, shuffle=True)
 
 
-# 動作の確認
-batch_iterator = iter(train_dataloader)  # イテレータに変換
-imges = next(batch_iterator)  # 1番目の要素を取り出す
-print(imges.size())  # torch.Size([64, 1, 64, 64])
-
-
-#create_model
-G = Generator(z_dim=20)
-D = Discriminator(z_dim=20)
-E = Encoder(z_dim=20)
-
-def train_model(G,D,E,dataloader,num_epochs):
-
-    # GPUが使えるかを確認
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print("使用デバイス：", device)
-
-    # 最適化手法の設定
-    lr_ge = 0.0001
-    lr_d = 0.0001/4
-    beta1, beta2 = 0.5, 0.999
-    g_optimizer = torch.optim.Adam(G.parameters(), lr_ge, [beta1, beta2])
-    e_optimizer = torch.optim.Adam(E.parameters(), lr_ge, [beta1, beta2])
-    d_optimizer = torch.optim.Adam(D.parameters(), lr_d, [beta1, beta2])
-
-    # 誤差関数を定義
-    # BCEWithLogitsLossは入力にシグモイド（logit）をかけてから、
-    # バイナリークロスエントロピーを計算
-    criterion = nn.BCEWithLogitsLoss(reduction='mean')
-
-    # パラメータをハードコーディング
-    z_dim = 20
-    mini_batch_size = 4
-
-    # ネットワークをGPUへ
-    G.to(device)
-    E.to(device)
-    D.to(device)
-
-    G.train()  # モデルを訓練モードに
-    E.train()  # モデルを訓練モードに
-    D.train()  # モデルを訓練モードに
-
-    # ネットワークがある程度固定であれば、高速化させる
-    torch.backends.cudnn.benchmark = True
-
-    # 画像の枚数
-    num_train_imgs = len(dataloader.dataset)
-    batch_size = dataloader.batch_size
-
-    # イテレーションカウンタをセット
-    iteration = 1
-    logs = []
-
-    # epochのループ
-    for epoch in range(num_epochs):
-
-        # 開始時刻を保存
-        t_epoch_start = time.time()
-        epoch_g_loss = 0.0  # epochの損失和
-        epoch_e_loss = 0.0  # epochの損失和
-        epoch_d_loss = 0.0  # epochの損失和
-
-        print('-------------')
-        print('Epoch {}/{}'.format(epoch, num_epochs))
-        print('-------------')
-        print('（train）')
-
-        # データローダーからminibatchずつ取り出すループ
-        for imges in dataloader:
-
-            # ミニバッチがサイズが1だと、バッチノーマライゼーションでエラーになるのでさける
-            if imges.size()[0] == 1:
-                continue
-
-            # ミニバッチサイズの1もしくは0のラベル役のテンソルを作成
-            # 正解ラベルと偽ラベルを作成
-            # epochの最後のイテレーションはミニバッチの数が少なくなる
-            mini_batch_size = imges.size()[0]
-            print("mini_batch_size",mini_batch_size)
-            label_real = torch.full((mini_batch_size,), fill_value=1.0).to(device)
-            print(label_real)
-            label_fake = torch.full((mini_batch_size,), fill_value=0.0).to(device)
-
-            # GPUが使えるならGPUにデータを送る
-            imges = imges.to(device)
-
-            # --------------------
-            # 1. Discriminatorの学習
-            # --------------------
-            # 真の画像を判定　
-            z_out_real = E(imges)
-            d_out_real, _ = D(imges, z_out_real)
-
-            # 偽の画像を生成して判定
-            input_z = torch.randn(mini_batch_size, z_dim).to(device)
-            fake_images = G(input_z)
-            d_out_fake, _ = D(fake_images, input_z)
-            print("hoge")
-            # 誤差を計算
-            d_loss_real = criterion(d_out_real.view(-1), label_real)
-            d_loss_fake = criterion(d_out_fake.view(-1), label_fake)
-            d_loss = d_loss_real + d_loss_fake
-
-            # バックプロパゲーション
-            d_optimizer.zero_grad()
-            d_loss.backward()
-            d_optimizer.step()
-
-            # --------------------
-            # 2. Generatorの学習
-            # --------------------
-            # 偽の画像を生成して判定
-            input_z = torch.randn(mini_batch_size, z_dim).to(device)
-            fake_images = G(input_z)
-            d_out_fake, _ = D(fake_images, input_z)
-
-            # 誤差を計算
-            g_loss = criterion(d_out_fake.view(-1), label_real)
-
-            # バックプロパゲーション
-            g_optimizer.zero_grad()
-            g_loss.backward()
-            g_optimizer.step()
-
-            # --------------------
-            # 3. Encoderの学習
-            # --------------------
-            # 真の画像のzを推定
-            z_out_real = E(imges)
-            d_out_real, _ = D(imges, z_out_real)
-
-            # 誤差を計算
-            e_loss = criterion(d_out_real.view(-1), label_fake)
-
-            # バックプロパゲーション
-            e_optimizer.zero_grad()
-            e_loss.backward()
-            e_optimizer.step()
-
-            # --------------------
-            # 4. 記録
-            # --------------------
-            epoch_d_loss += d_loss.item()
-            epoch_g_loss += g_loss.item()
-            epoch_e_loss += e_loss.item()
-            iteration += 1
-
-        # epochのphaseごとのlossと正解率
-        t_epoch_finish = time.time()
-        print('-------------')
-        print('epoch {} || Epoch_D_Loss:{:.4f} ||Epoch_G_Loss:{:.4f} ||Epoch_E_Loss:{:.4f}'.format(
-            epoch, epoch_d_loss/batch_size, epoch_g_loss/batch_size, epoch_e_loss/batch_size))
-        print('timer:  {:.4f} sec.'.format(t_epoch_finish - t_epoch_start))
-        t_epoch_start = time.time()
-
-    print("総イテレーション回数:", iteration)
-
-    return G, D, E
+# show train image and test image
+train_batch_iterator = iter(train_dataloader)
+test_batch_iterator = iter(test_dataloader)  # イテレータに変換
+train_images = next(train_batch_iterator)
+test_images = next(test_batch_iterator)  # 1番目の要素を取り出す
+#use matplot lib
+fig = plt.figure(figsize=(15, 6))
+## vew  test data
+for i in range(0, 5):
+    # upper columm is train data
+    plt.subplot(2, 5, i+1)
+    plt.imshow(train_images[i][0].cpu().detach().numpy(), 'gray')
+    # under columm is test data
+    plt.subplot(2, 5, 5+i+1)
+    plt.imshow(test_images[i][0].cpu().detach().numpy(), 'gray')
+    
+plt.savefig("view_traindata_test_data"+".pdf",dpi=500)
+#print(test_images.size())  # torch.Size([64, 1, 64, 64])
 
 #init network weight
 def weights_init(m):
@@ -248,6 +126,11 @@ def weights_init(m):
         m.bias.data.fill_(0)
 
 
+#create_model
+G = Generator(z_dim=20)
+D = Discriminator(z_dim=20)
+E = Encoder(z_dim=20)
+
 #adapt init func
 G.apply(weights_init)
 E.apply(weights_init)
@@ -255,8 +138,73 @@ D.apply(weights_init)
 print("init network weights!!")
 
 
-# 学習・検証を実行する
-# 15分ほどかかる
-num_epochs = 1500
-G_update, D_update, E_update = train_model(
-    G, D, E, dataloader=train_dataloader, num_epochs=num_epochs)
+# 学習・検証
+num_epochs = 150
+G_update, D_update, E_update = train.train_model(G, D, E, dataloader=train_dataloader, num_epochs=num_epochs)
+
+##test
+
+
+def Anomaly_score(x, fake_img, z_out_real, D, Lambda=0.1):
+
+    # テスト画像xと生成画像fake_imgのピクセルレベルの差の絶対値を求めて、ミニバッチごとに和を求める
+    residual_loss = torch.abs(x-fake_img)
+    residual_loss = residual_loss.view(residual_loss.size()[0], -1)
+    residual_loss = torch.sum(residual_loss, dim=1)
+
+    # テスト画像xと生成画像fake_imgを識別器Dに入力し、特徴量マップを取り出す
+
+    _, x_feature = D(x, z_out_real)
+    _, G_feature = D(fake_img, z_out_real)
+
+    # テスト画像xと生成画像fake_imgの特徴量の差の絶対値を求めて、ミニバッチごとに和を求める
+    discrimination_loss = torch.abs(x_feature-G_feature)
+    discrimination_loss = discrimination_loss.view(
+        discrimination_loss.size()[0], -1)
+    discrimination_loss = torch.sum(discrimination_loss, dim=1)
+
+    # ミニバッチごとに2種類の損失を足し算する
+    loss_each = (1-Lambda)*residual_loss + Lambda*discrimination_loss
+
+    # ミニバッチ全部の損失を求める
+    total_loss = torch.sum(loss_each)
+
+    return total_loss, loss_each, residual_loss
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("使用デバイス：", device)
+
+# 異常検知したい画像
+x = test_images[0:5]
+x = x.to(device)
+
+# 教師データの画像をエンコードしてzにしてから、Gで生成
+E_update.eval()
+G_update.eval()
+z_out_real = E_update(test_images.to(device))
+imges_reconstract = G_update(z_out_real)
+
+# 損失を求める
+loss, loss_each, residual_loss_each = Anomaly_score(
+    x, imges_reconstract, z_out_real, D_update, Lambda=0.1)
+
+# 損失の計算。トータルの損失
+loss_each = loss_each.cpu().detach().numpy()
+print("total loss：", np.round(loss_each, 0))
+
+# 画像を可視化
+fig = plt.figure(figsize=(15, 6))
+for i in range(0, 5):
+    # 上段に訓練データを
+    plt.subplot(3, 5, i+1)
+    plt.imshow(train_images[i][0].cpu().detach().numpy(), 'gray')
+    
+    plt.subplot(3, 5, 5+i+1)
+    plt.imshow(test_images[i][0].cpu().detach().numpy(), 'gray')
+    
+    # 下段に生成データを表示する
+    plt.subplot(3, 5, 10+i+1)
+    plt.imshow(imges_reconstract[i][0].cpu().detach().numpy(), 'gray')
+    
+plt.savefig("test_compare"+".png",dpi=500)
