e7a1b5dd360631f2c64426e3701c4e621d1d374d
haxhimitsu <hachimitsu.lab@gmail.com>
2021-02-08 17:07:01 +0900

    mody sample numver

--- torch_ver/src/check_torch.py
+++ torch_ver/src/check_torch.py
@@ -8,7 +8,7 @@ def save(data, index, num):
     for i in range(28):
         for j in range(28):
             pix[i, j] = int(data[i+j*28]*256)
-    img2 = img.resize((280, 280))
+    img2 = img.resize((28, 28))
     filename = str(num) + "/test" + "{0:04d}".format(index) + ".png"
     img2.save(filename)
     print (filename)
@@ -19,7 +19,7 @@ def main():
         dirname = str(i)
         if os.path.isdir(dirname) is False:
             os.mkdir(dirname)
-    for i in range(5000):
+    for i in range(10000):
         save(test[i][0], i, test[i][1])
 
 if __name__ == '__main__':
