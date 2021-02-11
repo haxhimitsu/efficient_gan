d7e45a7ab655e6335a86443c0a19d8d104ce356b
haxhimitsu <hachimitsu.lab@gmail.com>
2021-02-11 16:54:40 +0900

    change exclude argmets

--- torch_ver/src/ex.sh
+++ torch_ver/src/ex.sh
@@ -7,7 +7,7 @@
 
 
 python3 efficient_GAN_torch.py  --dataset_path ~/Desktop/nagase_fft/01/ --mode train  --log_dir ./log_fft01/
-python3 efficient_GAN_torch.py  --dataset_path ~/Desktop/nagase_fft/01/ --mode test --result_dir ./log_fft01/01/
-python3 efficient_GAN_torch.py  --dataset_path ~/Desktop/nagase_fft/02/ --mode test --result_dir ./log_fft01/02/
-python3 efficient_GAN_torch.py  --dataset_path ~/Desktop/nagase_fft/03/ --mode test --result_dir ./log_fft01/03/
-python3 efficient_GAN_torch.py  --dataset_path ~/Desktop/nagase_fft/04/ --mode test --result_dir ./log_fft01/04/
+python3 efficient_GAN_torch.py  --dataset_path ~/Desktop/nagase_fft/01/ --mode test --result_dir ./log_fft01/01/ --log_dir ./log_fft01/
+python3 efficient_GAN_torch.py  --dataset_path ~/Desktop/nagase_fft/02/ --mode test --result_dir ./log_fft01/02/ --log_dir ./log_fft01/
+python3 efficient_GAN_torch.py  --dataset_path ~/Desktop/nagase_fft/03/ --mode test --result_dir ./log_fft01/03/ --log_dir ./log_fft01/
+python3 efficient_GAN_torch.py  --dataset_path ~/Desktop/nagase_fft/04/ --mode test --result_dir ./log_fft01/04/ --log_dir ./log_fft01/
